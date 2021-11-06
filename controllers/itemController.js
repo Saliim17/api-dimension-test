a23ef5eca33c1e30c3a17808cabef5f9607a03cf
Saliim17 <ahouchsalim@gmail.com>
2021-11-06 01:03:18 +0000

    updated controllers

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -50,18 +50,6 @@ function updateItem(req, res) { // PUT item
   });
 }
 
-function checkItemStock(itemId) {
-
-  // Busca el objecto para asegurarse de que existe
-  Item.findOne({ id: itemId }, (err, itm) => {
-    if (err) return err;
-    if (!itm) return `Item with id ${itemId} not found. Please, create it first.`;
-
-    //El objeto existe, devolvemos su stock.
-    return itm.stock;
-  });
-}
-
 module.exports = {
   getItems,
   getItemsById,
