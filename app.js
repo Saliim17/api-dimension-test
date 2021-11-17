d1bc2405a4ad3607b8244c56320b8c6659df8c4e
Saliim17 <ahouchsalim@gmail.com>
2021-11-17 16:09:07 +0100

    Updated dependencies

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require('express');
-
-const userRoutes = require('./routes/userRoutes');
-const activitiesRoutes = require('./routes/activityRoutes');
-const itemsRoutes = require('./routes/itemRoutes');
-
-const app = express();
-
+const express = require("express"),
+    session = require('express-session'),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/users"),
+    dotenv = require('dotenv'),
+    userRoutes = require('./routes/userRoutes'),
+    activitiesRoutes = require('./routes/activityRoutes'),
+    itemsRoutes = require('./routes/itemRoutes'),
+    authRoutes = require('./routes/authRoutes'),
+    app = express();
+    
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/activities', activitiesRoutes);
 app.use('/api/items', itemsRoutes);
-
+app.use('/api/auth', authRoutes);    
 module.exports = app;
+    
+    
