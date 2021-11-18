6e2b8f24def39ced10ae558c1a86f9a8a7e71585
Saliim17 <ahouchsalim@gmail.com>
2021-11-18 13:44:03 +0100

    Update token functions

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken');
-
+const dotenv = require('dotenv'); 
+dotenv.config(); 
+const KEY = process.env.KEY;
 function verifyToken(req, res, next) {
   const { token } = req.headers;
 
-  jwt.verify(token, process.env.KEY, (err) => {
+  jwt.verify(token, KEY, (err) => {
     if (err) return res.status(401).send({ err });
     next();
   });
 }
 
-function createToken(userId) {
+function createToken(email) {
   return jwt.sign({
-    data: userId,
-  }, process.env.KEY, { expiresIn: 60 * 60 });
+    data: email,
+  }, KEY, { expiresIn: 60 * 60 });
 }
 
 module.exports = {
